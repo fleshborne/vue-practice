8ff8bd7777e0577be1c0216b2a259932b161a76f
Fleshborne <blakeathompson94@gmail.com>
2020-08-18 19:21:59 -0400

    doing some vue things

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <p>{{ message.toUpperCase() }}</p>
+    <label>Message:</label>
+    <!-- v-model is the same hooking components together  -->
+    <input type="text" v-model="message" />
+    <table class="responsive-table" align="center">
+      {{
+        todos
+      }}
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  data: function() {
+    return {
+      message: 'this is the message that will be displayed.',
+      todos: ['Clean', 'Do the Thing', 'Eat', 'Sleep'],
+    };
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
 // wow new vue stuff
