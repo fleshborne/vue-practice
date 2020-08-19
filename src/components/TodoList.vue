0573294387dbcdd7b724f4e0d6875ffa1db4b895
Fleshborne <blakeathompson94@gmail.com>
2020-08-18 21:12:03 -0400

    finished with lesson

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h1>{{ todosCount }} Todo Items</h1>
     <ul>
       <li v-for="todo in todos" :key="todo">
         {{ todo }}
@@ -14,6 +15,17 @@
 export default {
   name: 'TodoList',
   props: ['todos'],
+  computed: {
+    todosCount: function() {
+      return this.todos.length;
+    },
+  },
+  //   if you were to pull this todo list from an api, below is how you'd do it.
+  //   created: function() {
+  //     axios.get('/todos').then((res) => {
+  //       this.todos = res.data;
+  //     });
+  //   },
 };
 </script>
 
